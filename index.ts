f440421c80cc764584236d27b72a26bfd2638bfe
Junaid Ali <gunaidali75@gmail.com>
2023-11-07 06:31:43 -0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+
+type ansType ={
+    userGuess: number
+}
+
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+
+console.log(systemGeneratedNo)
+
+const answers : ansType = await inquirer.prompt([
+  {
+    type: "number",
+    name: "userGuess",
+    message: "Write Your Guess between 1 to 10:",
+
+  },
+
+  
+])
+
+// console.log(answers.userGuess)
+
+const {userGuess} = answers;
+
+console.log(userGuess, "userGuess", systemGeneratedNo, 'Sys')
+if(userGuess === systemGeneratedNo){
+        console.log("Yes Its a Correct Answer")
+}
+else{
+    console.log("Please Try Again & Better Luck Next Time")
+}
